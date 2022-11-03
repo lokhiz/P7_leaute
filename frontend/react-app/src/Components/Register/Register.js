0b329293253ddf1965f89a56e1f2ff67d00d41cf
Etienne Leaute <l.etienne2635@gmail.com>
2022-11-03 11:18:11 +0100

    Confirm Password set

--- frontend/react-app/src/Components/Register/Register.js
+++ frontend/react-app/src/Components/Register/Register.js
@@ -1,40 +1,10 @@
 import "./Register.css";
-import logo from "../../assets/icon-left-font.png";
-import { useForm } from "react-hook-form";
+import { ConfirmPassword } from "../ConfirmPassword";
 
 export function Register() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
-
   return (
     <div className="Form">
-      <img src={logo} alt="Groupomania" className="logo" />
-      <div className="inputs">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="email"
-            placeholder="Email :"
-            {...register("email")}
-            required
-          />
-
-          <input
-            type="password"
-            placeholder="Mot de passe :"
-            {...register("password")}
-            required
-          />
-
-          <input
-            type="password"
-            placeholder="Confirmation du mot de passe :"
-            {...register("confirmPassword")}
-            required
-          />
-
-          <button type="submit">Inscription</button>
-        </form>
-      </div>
+      <ConfirmPassword />
     </div>
   );
 }
