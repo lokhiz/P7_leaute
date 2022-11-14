df85e99dbb4eef7d1311981b463cae99e033e24f
Etienne Leaute <l.etienne2635@gmail.com>
2022-11-14 12:09:45 +0100

    .

--- frontend/react-app/src/Components/Register/Register.js
+++ frontend/react-app/src/Components/Register/Register.js
@@ -1,23 +1,8 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
 import axios from "axios";
 import logo from "../../assets/icon-left-font.png";
 
 export function Register() {
-  const formSchema = Yup.object().shape({
-    password: Yup.string()
-      .required("Mot de passe requis")
-      .min(3, "3 caractères minimum requis"),
-    confirmPwd: Yup.string()
-      .required("Mot de passe requis")
-      .oneOf([Yup.ref("password")], "Les mots de passe ne correspondent pas"),
-  });
-  const formOptions = { resolver: yupResolver(formSchema) };
-  const { register, handleSubmit, formState } = useForm(formOptions);
-  const { errors } = formState;
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -41,9 +26,10 @@ export function Register() {
         name="email"
         type="email"
         placeholder="Email :"
-        {...register("email")}
         value={email}
-        onChange={(e) => {setEmail(e.target.value)}}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
         required
       />
       <div className="form-group">
@@ -51,22 +37,11 @@ export function Register() {
           name="password"
           type="password"
           placeholder="Mot de passe :"
-          {...register("password")}
           value={password}
-          onChange={(e) => {setPassword(e.target.value)}}
-          className={`form-control ${errors.password ? "is-invalid" : ""}`}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
-        <div className="invalid-feedback">{errors.password?.message}</div>
-      </div>
-      <div className="form-group">
-        <input
-          name="confirmPwd"
-          type="password"
-          placeholder="Confirmez le mot de passe"
-          {...register("confirmPwd")}
-          className={`form-control ${errors.confirmPwd ? "is-invalid" : ""}`}
-        />
-        <div className="invalid-feedback">{errors.confirmPwd?.message}</div>
       </div>
       <button type="submit">Inscription</button>
     </form>
