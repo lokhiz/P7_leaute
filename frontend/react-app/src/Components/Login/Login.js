d7ab028f374f58f2f4ee4367495b2e77011bc99d
Etienne Leaute <l.etienne2635@gmail.com>
2022-11-15 15:45:48 +0100

    login set

--- frontend/react-app/src/Components/Login/Login.js
+++ frontend/react-app/src/Components/Login/Login.js
@@ -1,21 +1,50 @@
 import "./Login.css";
+import axios from "axios";
 import logo from "../../assets/icon-left-font.png";
-import { useForm } from 'react-hook-form';
+import { useState } from "react";
 
 export function Login() {
-  const {login, setLogin} = useForm()
-  const onSubmit = data => console.log(data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function getUserData(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/api/auth/login", {
+        email: email,
+        password: password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <div className="Form">
+    <div className="Form" onSubmit={getUserData}>
       <img src={logo} alt="Groupomania" className="logo" />
       <div className="inputs">
         <form>
-          <input type="text" name="email" placeholder="Email :" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email :"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
 
-          <input type="password" name="password" placeholder="Mot de passe :" />
-
-          <button>Connexion</button>
+          <input
+            type="password"
+            name="password"
+            placeholder="Mot de passe :"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button type="submit">Connexion</button>
         </form>
       </div>
     </div>
