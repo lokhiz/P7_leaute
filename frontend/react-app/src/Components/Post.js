28a5752394ceddc9d21720b2f2099a3dd3fb3c1a
Etienne Leaute <l.etienne2635@gmail.com>
2023-01-02 16:19:44 +0100

    css changes

--- frontend/react-app/src/Components/Post.js
+++ frontend/react-app/src/Components/Post.js
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-    const PF = "http://localhost:5000/images/"
-    return (
-        <div className="post">
-            {post.photo && <img className="postImg" src={PF + post.photo} alt="post" />}
-            <div className="postInfo">
-                <Link to={`/post/${post._id}`} className="link">
-                    <span className="postTitle">{post.title}</span>
-                </Link>
-            </div>
-            <p className="postDesc">{post.desc}</p>
-        </div>
-    )
+  const PF = "http://localhost:5000/images/";
+  return (
+    <div className="post">
+      {post.photo && (
+        <img className="postImg" src={PF + post.photo} alt="post" />
+      )}
+      <div className="postInfo">
+        <Link to={`/post/${post._id}`} className="link">
+          <h1 className="postTitle">{post.title}</h1>
+        </Link>
+        <p className="postDesc">{post.desc}</p>
+      </div>
+    </div>
+  );
 }
