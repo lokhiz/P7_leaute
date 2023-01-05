848f7748f2e165857b30f211bdd64119da5e1846
Etienne Leaute <l.etienne2635@gmail.com>
2023-01-05 13:37:12 +0100

    files updates

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
-// const multer = require("../middleware/multer-config");
 
 const postsCtrl = require("../controllers/posts");
 
